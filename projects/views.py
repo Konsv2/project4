1658e64fc24d50a106e8f3fb23d1304b4f770a52
Konsv2 <88188874+Konsv2@users.noreply.github.com>
2022-09-13 09:16:16 +0300

    Update views.py

--- projects/views.py
+++ projects/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from projects.models import Project
 
 def project_index(request):
-    projects = Project.objects.order_by('?')[0]
+    projects = Project.objects.all().order_by('?')[0]
     context = {
         'projects': projects
     }
