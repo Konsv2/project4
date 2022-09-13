4077b1936313f72624bcbd50f42e6fbc6e767b33
Konsv2 <88188874+Konsv2@users.noreply.github.com>
2022-09-13 10:21:50 +0300

    Update views.py

--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from projects.models import Project
+import random
+
+items = list(Product.objects.all())
+
+# change 3 to how many random items you want
+random_items = random.sample(items, 6)
+# if you want only a single random item
+random_item = random.choice(items)
+
 
 def project_index(request):
-    projects = Project.objects.order_by('?')[0]
+    
+    items = list(Product.objects.all())
+
+    # change 3 to how many random items you want
+    random_items = random.sample(items, 6)
+    # if you want only a single random item
+    projects = random.choice(items)
+    
+   # projects = Project.objects.order_by('?')[0]
+
     context = {
         'projects': projects
     }
